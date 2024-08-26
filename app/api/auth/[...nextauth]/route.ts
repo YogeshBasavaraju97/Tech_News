91429f988df940457bd217b536a84ce61d4abf69
Yogesh Basavaraju <w1853885@westminster.ac.uk>
2024-08-26 01:45:21 +0100

    Adding Next Auth

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,25 @@
+import nextAuth, { AuthOptions } from 'next-auth';
+
+import GithubProvider from 'next-auth/providers/github';
+import GoogleProvider from 'next-auth/providers/google';
+
+const authOptions: AuthOptions = {
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+  pages: {
+    signIn: '/sign-In',
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
+
+const handler = nextAuth(authOptions);
+
+export { handler as GET, handler as POST };
