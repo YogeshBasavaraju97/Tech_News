50f76563c9d617070027e3d8b66e617b8a8b1d22
Yogesh Basavaraju <w1853885@westminster.ac.uk>
2024-10-26 01:50:41 +0530

    ew

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import prisma from '@/lib/prismadb';
 import { getServerSession } from 'next-auth';
-import { authOptions } from '../auth/[...nextauth]/route';
+import { authOptions } from '@/lib/auth';
 
 export async function POST(req: Request) {
   const session = await getServerSession(authOptions);
