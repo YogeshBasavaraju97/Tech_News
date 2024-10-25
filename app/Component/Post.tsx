b5fa7688ffff47823d6792310ea20d2e4e73efd2
Yogesh Basavaraju <w1853885@westminster.ac.uk>
2024-10-26 00:08:42 +0530

    Image changed to image

--- app/Component/Post.tsx
+++ app/Component/Post.tsx
@@ -1,5 +1,5 @@
 import defaultThumbnail from '../../public/defaultThumbnail.png';
-import Image from 'next/Image';
+import Image from 'next/image';
 import Link from 'next/link';
 import DeleteButton from './DeleteButton';
 import { getServerSession } from 'next-auth';
