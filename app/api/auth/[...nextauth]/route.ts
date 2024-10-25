fde37480253c77e58cbea72c0a724c055eff52fc
Yogesh Basavaraju <w1853885@westminster.ac.uk>
2024-10-26 02:58:16 +0530

    updated on Authentication

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -4,10 +4,4 @@ import { NextRequest } from 'next/server';
 
 const handler = NextAuth(authOptions);
 
-export async function GET(req: NextRequest) {
-  return handler(req);
-}
-
-export async function POST(req: NextRequest) {
-  return handler(req);
-}
+export { handler as GET, handler as POST };
