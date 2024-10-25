2c567c67638ef4dd07806b5c0a99cd6f30bed965
Yogesh Basavaraju <w1853885@westminster.ac.uk>
2024-10-26 01:43:24 +0530

    changed

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,35 +1,13 @@
-import NextAuth, { NextAuthOptions } from 'next-auth';
-
-import GithubProvider from 'next-auth/providers/github';
-import GoogleProvider from 'next-auth/providers/google';
-import { PrismaAdapter } from '@auth/prisma-adapter';
-import prisma from '@/lib/prismadb';
-import { NextRequest, NextResponse } from 'next/server';
-
-export const authOptions: NextAuthOptions = {
-  adapter: PrismaAdapter(prisma),
-  providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_CLIENT_ID as string,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
-    }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    }),
-  ],
-  pages: {
-    signIn: '/sign-In',
-  },
-  secret: process.env.NEXTAUTH_SECRET,
-};
+import NextAuth from 'next-auth';
+import { authOptions } from '@/lib/auth';
+import { NextRequest } from 'next/server';
 
 const handler = NextAuth(authOptions);
 
 export async function GET(req: NextRequest) {
-  return handler(req) as Promise<NextResponse>;
+  return handler(req);
 }
 
 export async function POST(req: NextRequest) {
-  return handler(req) as Promise<NextResponse>;
+  return handler(req);
 }
---
+++ lib/auth.ts
@@ -0,0 +1,25 @@
+import { NextAuthOptions } from 'next-auth';
+
+import GithubProvider from 'next-auth/providers/github';
+import GoogleProvider from 'next-auth/providers/google';
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import prisma from '@/lib/prismadb';
+import { NextRequest, NextResponse } from 'next/server';
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+  pages: {
+    signIn: '/sign-In',
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
